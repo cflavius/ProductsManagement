81f3970873a12fc23ecbe3160a4c0549e7b62915
cflavius <cflavius7@gmail.com>
2020-07-07 10:53:55 +0300

    refactoring

--- src/main/java/com/flavius/productsmanagement/controllers/ProductsController.java
+++ src/main/java/com/flavius/productsmanagement/controllers/ProductsController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flavius.productsmanagement.model.Product;
@@ -43,7 +41,7 @@ public class ProductsController {
 	}
 	
 	@PatchMapping("/products/{id}")
-	public Product updaProduct(@PathVariable int id, @RequestBody Product product) {
+	public Product updateProduct(@PathVariable int id, @RequestBody Product product) {
 		return productsService.updateProduct(id, product);
 	}
 	
